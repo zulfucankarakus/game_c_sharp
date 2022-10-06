89855d51df7e35a1fc48318481bbf138fd705966
Zülfücan Karakuş <84273117+zulfucankarakus@users.noreply.github.com>
2022-10-06 20:15:51 +0300

    Create MenuManagerMenuScenes.cs

---
+++ MenuManagerMenuScenes.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManagerMenuScenes : MonoBehaviour
+{
+    public GameObject dataBoard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void DataBoardButton()
+    {
+        
+        dataBoard.transform.GetChild(2).GetComponent<Text>().text = DataManager.Instance.totalEnemyKilled.ToString();
+        dataBoard.SetActive(true);
+    }
+    public void XButton()
+    {
+        dataBoard.SetActive(false);
+    }
+}
